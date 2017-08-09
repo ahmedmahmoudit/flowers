414783de192f138a53a0df599b55b46e77dd7b90
ZaL <z4ls@live.com>
2017-08-09 13:58:53 +0300

    fix modal bug in mobile

--- resources/views/products/view.blade.php
+++ resources/views/products/view.blade.php
@@ -191,7 +191,7 @@
                                                         </span>
                                                     </span>
 
-                                                    <div class="modal fade" id="time-picker-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                                    <div class="modal" id="time-picker-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                                                         <div class="modal-dialog ">
                                                             <div class="modal-content c-square">
                                                                 <div class="modal-header">
