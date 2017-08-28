523ae77d3328725ab8d3c58380e6707039311e0a
ZaL <z4ls@live.com>
2017-08-28 14:11:52 +0300

    add Contact Mail

---
+++ app/Mail/Contact.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class Contact extends Mailable
+{
+    use Queueable, SerializesModels;
+    /**
+     * @var array
+     */
+    private $params;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param array $params
+     */
+    public function __construct(array $params)
+    {
+        $this->params = $params;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.contact',$this->params);
+    }
+
+}
