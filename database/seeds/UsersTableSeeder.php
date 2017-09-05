f1639a8b502a00ecea7fab5869e6394908bb6fae
ZaL <z4ls@live.com>
2017-09-06 00:12:23 +0300

    seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -23,20 +23,20 @@ class UsersTableSeeder extends Seeder
             'api_token' => str_random(60),
         ]);
 
-        //Admin User
-        DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@test.com',
-            'password' => bcrypt('password'),
-            'role' => '2', // manager
-            'api_token' => str_random(60),
-            'store_id' => \App\Store::all()->random()->id
-        ]);
-
-        factory(App\User::class, 1)->create([
-            'role' => '3',
-            'email' => 'user@test.com'
-        ]);
+//        //Admin User
+//        DB::table('users')->insert([
+//            'name' => 'admin',
+//            'email' => 'admin@test.com',
+//            'password' => bcrypt('password'),
+//            'role' => '2', // manager
+//            'api_token' => str_random(60),
+//            'store_id' => \App\Store::all()->random()->id
+//        ]);
+//
+//        factory(App\User::class, 1)->create([
+//            'role' => '3',
+//            'email' => 'user@test.com'
+//        ]);
 
         $this->command->info('Users Seeded!');
     }
