be6bca6e9b8052ff3765f86a922d20700e2d111b
ZaL <z4ls@live.com>
2017-09-06 09:17:55 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
+Route::get('test',function() {
+
+//    return Mail::to('z4ls@live.com');
+
+    $data = ['name' =>"Virat Gandhi"];
+
+    Mail::send(['text'=>'mail'], $data, function($message) {
+        $message->to('z4ls@live.com', 'Tutorials Point')->subject
+        ('Laravel Basic Testing Mail');
+        $message->from('xyz@gmail.com','Virat Gandhi');
+    });
+    echo "Basic Email Sent. Check your inbox.";
+});
 
 Route::get('manager/login', function () {
     Auth::logout();
