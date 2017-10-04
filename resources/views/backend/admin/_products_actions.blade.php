0d60db418a92e56518367f0bc51d0300d2adc0c0
ZaL <z4ls@live.com>
2017-10-04 14:26:32 +0300

    adjust image size

--- resources/views/backend/admin/_products_actions.blade.php
+++ resources/views/backend/admin/_products_actions.blade.php
@@ -1,8 +1,8 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<a href="{{ route('admin.products.edit', $product->id) }}" class="btn bg-orange margin ">Edit</a>
-<a href="{{ route('admin.products.destroy', $product->id) }}" data-method="POST" data-laravel-method="delete" class="btn bg-red confirm-delete">Delete</a>
+<a href="{{ route('admin.products.edit', $product->id) }}" class="btn btn-xs bg-orange margin ">Edit</a>
+<a href="{{ route('admin.products.destroy', $product->id) }}" data-method="POST" data-laravel-method="delete" class="btn btn-xs bg-red confirm-delete">Delete</a>
 @if($product->active == '1')
-    <a href="{{ route('admin.products.disable', $product->id) }}" data-method="POST" data-laravel-method="post" class="btn bg-red confirm-disable">Disable</a>
+    <a href="{{ route('admin.products.disable', $product->id) }}" data-method="POST" data-laravel-method="post" class="btn btn-xs bg-red confirm-disable">Disable</a>
 @else
-    <a href="{{ route('admin.products.activate', $product->id) }}" data-method="POST" data-laravel-method="post" class="btn bg-green confirm-activate">Activate</a>
+    <a href="{{ route('admin.products.activate', $product->id) }}" data-method="POST" data-laravel-method="post" class="btn btn-xs bg-green confirm-activate">Activate</a>
 @endif
