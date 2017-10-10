09755f1a467cca6cdfe84541cf3bbda2b4e41ac0
ZaL <z4ls@live.com>
2017-10-10 16:59:56 +0300

    edit

--- resources/views/backend/manager/pages/terms.blade.php
+++ resources/views/backend/manager/pages/terms.blade.php
@@ -18,28 +18,28 @@
                             <div class="col-xs-6">
                                 <div class="form-group">
                                     <label for="exampleInputTitle">{{__('adminPanel.english_title')}}</label>
-                                    {!! Form::text('title_en', (isset($termsData) ? $termsData->title_en : old('title_en')) ,['class' => 'form-control','required']) !!}
+                                    {!! Form::text('title_en', (isset($termsData) ? $termsData->title_en : old('title_en')) ,['class' => 'form-control']) !!}
                                     <p class="help-block"></p>
                                 </div>
                             </div>
                             <div class="col-xs-6">
                                 <div class="form-group">
                                     <label for="exampleInputTitleAr">{{__('adminPanel.arabic_title')}}</label>
-                                    {!! Form::text('title_ar', (isset($termsData) ? $termsData->title_ar : old('title_ar')) ,['class' => 'form-control','required']) !!}
+                                    {!! Form::text('title_ar', (isset($termsData) ? $termsData->title_ar : old('title_ar')) ,['class' => 'form-control']) !!}
                                     <p class="help-block"></p>
                                 </div>
                             </div>
                             <div class="col-xs-12">
                                 <div class="form-group">
                                     <label for="exampleInputBodyEn">{{__('adminPanel.english_body')}}</label>
-                                    {!! Form::textarea('body_en', (isset($termsData) ? $termsData->body_en : old('body_en')) ,['class' => 'form-control','rows editor_en' => 20, 'cols' => 100,'required']) !!}
+                                    {!! Form::textarea('body_en', (isset($termsData) ? $termsData->body_en : old('body_en')) ,['class' => 'form-control  editor_en','rows' => 10]) !!}
                                     <p class="help-block"></p>
                                 </div>
                             </div>
                             <div class="col-xs-12">
                                 <div class="form-group">
                                     <label for="exampleInputBodyAr">{{__('adminPanel.arabic_body')}}</label>
-                                    {!! Form::textarea('body_ar', (isset($termsData) ? $termsData->body_ar : old('body_ar')) ,['class' => 'form-control editor_ar','rows' => 20, 'cols' => 100,'required']) !!}
+                                    {!! Form::textarea('body_ar', (isset($termsData) ? $termsData->body_ar : old('body_ar')) ,['class' => 'form-control editor_ar','rows' => 10]) !!}
                                     <p class="help-block"></p>
                                 </div>
                             </div>
