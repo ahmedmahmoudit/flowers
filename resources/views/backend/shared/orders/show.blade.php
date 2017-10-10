d1d268582c4d3a2c7b133838c2bbfd740e1796e8
ZaL <z4ls@live.com>
2017-10-10 19:02:02 +0300

    store

--- resources/views/backend/shared/orders/show.blade.php
+++ resources/views/backend/shared/orders/show.blade.php
@@ -168,9 +168,9 @@
                                 @if($order->orderDetails && $order->orderDetails->count() )
                                     @foreach($order->orderDetails as $item)
                                         <tr>
-                                            <td>{{$item->product->sku}}</td>
-                                            <td>{{$item->product->name_en}}</td>
-                                            {{--<td>{{($item->sale_price ? $item->sale_price : $item->price)}}</td>--}}
+                                            {{--<td>{{$item->product->sku}}</td>--}}
+                                            {{--<td>{{$item->product->name_en}}</td>--}}
+                                            <td>{{($item->sale_price ? $item->sale_price : $item->price)}}</td>
                                             {{--<td>{{$item->product->detail->height or 'No Height'}}--}}
                                                 {{--/ {{$item->product->detail->width or 'No Width'}}</td>--}}
                                             {{--<td>{{$item->quantity}}</td>--}}
