ca5311e44e78897ad78217b13c5ca0cb700a0150
ZaL <z4ls@live.com>
2017-10-18 18:14:48 +0300

    add protfolio

--- public/js/components.js
+++ public/js/components.js
@@ -194,6 +194,118 @@ var ContentDatePickers = function () {
 }();
 // END : DATEPICKERS
 
+
+// BEGIN: ContentCubeLatestPortfolio
+var ContentCubeLatestPortfolio = function () {
+
+  var _initInstances = function () {
+
+    // init cubeportfolio
+    $('.c-content-latest-works').cubeportfolio({
+      filters: '#filters-container',
+      loadMore: '#loadMore-container',
+      loadMoreAction: 'click',
+      layoutMode: 'grid',
+      defaultFilter: '*',
+      animationType: 'quicksand',
+      gapHorizontal: 20,
+      gapVertical: 23,
+      gridAdjustment: 'responsive',
+      mediaQueries: [{
+        width: 1100,
+        cols: 4
+      }, {
+        width: 800,
+        cols: 3
+      }, {
+        width: 500,
+        cols: 2
+      }, {
+        width: 320,
+        cols: 1
+      }],
+      caption: 'zoom',
+      displayType: 'lazyLoading',
+      displayTypeSpeed: 100,
+
+      // lightbox
+      lightboxDelegate: '.cbp-lightbox',
+      lightboxGallery: true,
+      lightboxTitleSrc: 'data-title',
+      lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+
+      // singlePage popup
+      singlePageDelegate: '.cbp-singlePage',
+      singlePageDeeplinking: true,
+      singlePageStickyNavigation: true,
+      singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
+      singlePageCallback: function (url, element) {
+        // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+        var t = this;
+
+        $.ajax({
+          url: url,
+          type: 'GET',
+          dataType: 'html',
+          timeout: 5000
+        })
+          .done(function (result) {
+            t.updateSinglePage(result);
+          })
+          .fail(function () {
+            t.updateSinglePage("Error! Please refresh the page!");
+          });
+      },
+    });
+
+    $('.c-content-latest-works-fullwidth').cubeportfolio({
+      loadMoreAction: 'auto',
+      layoutMode: 'grid',
+      defaultFilter: '*',
+      animationType: 'fadeOutTop',
+      gapHorizontal: 0,
+      gapVertical: 0,
+      gridAdjustment: 'responsive',
+      mediaQueries: [{
+        width: 1600,
+        cols: 5
+      }, {
+        width: 1200,
+        cols: 4
+      }, {
+        width: 800,
+        cols: 3
+      }, {
+        width: 500,
+        cols: 2
+      }, {
+        width: 320,
+        cols: 1
+      }],
+      caption: 'zoom',
+      displayType: 'lazyLoading',
+      displayTypeSpeed: 100,
+
+      // lightbox
+      lightboxDelegate: '.cbp-lightbox',
+      lightboxGallery: true,
+      lightboxTitleSrc: 'data-title',
+      lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+    });
+
+  };
+
+  return {
+
+    //main function to initiate the module
+    init: function () {
+      _initInstances();
+    }
+
+  };
+}();
+// END: ContentCubeLatestPortfolio
+
 // Main theme initialization
 $(document).ready(function () {
   LayoutBrand.init();
@@ -203,4 +315,5 @@ $(document).ready(function () {
   LayoutHeader.init();
 
   ContentDatePickers.init();
+  ContentCubeLatestPortfolio.init();
 });
