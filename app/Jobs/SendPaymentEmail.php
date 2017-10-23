ebf28c2e035e5eae41046b36eac9ffc66e14c9fa
ZaL <z4ls@live.com>
2017-10-23 13:59:59 +0300

    new changes

--- app/Jobs/SendPaymentEmail.php
+++ app/Jobs/SendPaymentEmail.php
@@ -43,50 +43,50 @@ class SendPaymentEmail
         $this->sendStoreCopy();
     }
 
-    private function sendCustomerCopy() {
+    private function sendCustomerCopy()
+    {
 
         $emailBody = [];
         $mailer = $this->mailer;
 
         $mailer->view = app()->getLocale() === 'ar' ? 'emails.payment_success_customer_v1_ar' : 'emails.payment_success_customer_v1_en';
 
-        $mailer->subject =  trans('general.payment_mail_success_subject') ;
-        $mailer->toEmail =  $this->order->email;
-        $mailer->toName = $this->order->firstname . ' ' .$this->order->lastname;
+        $mailer->subject = trans('general.payment_mail_success_subject');
+        $mailer->toEmail = $this->order->email;
+        $mailer->toName = $this->order->firstname . ' ' . $this->order->lastname;
 
         $emailBody['id'] = $this->order->id;
         $emailBody['invoice_id'] = $this->order->invoice_id;
         $emailBody['amount'] = $this->order->sale_amount;
-        $emailBody['name'] = $this->order->firstname . ' ' .$this->order->lastname;
+        $emailBody['name'] = $this->order->firstname . ' ' . $this->order->lastname;
         $emailBody['mobile'] = $this->order->mobile;
-        $emailBody['recipient_name'] = $this->order->recipient_firstname . ' ' .$this->order->recipient_lastname;
+        $emailBody['recipient_name'] = $this->order->recipient_firstname . ' ' . $this->order->recipient_lastname;
         $emailBody['recipient_mobile'] = $this->order->recipient_mobile;
         $emailBody['order_date'] = $this->order->created_at->format('d-m-Y');
         $emailBody['order_notes'] = $this->order->order_notes;
         $emailBody['card_notes'] = $this->order->card_notes;
 
 
-        if($address = $this->order->address) {
-            $emailBody['area'] =  $address->area ? $address->area->name : '-';
-            $emailBody['block'] =  $address->block ? $address->block : '';
-            $emailBody['street'] =  $address->street ? $address->street : '';
-            $emailBody['house'] =  $address->house ? $address->house : '';
+        if ($address = $this->order->address) {
+            $emailBody['area'] = $address->area ? $address->area->name . ' ' : '-';
+            $emailBody['block'] = $address->block ? $address->block . ' ' : '';
+            $emailBody['street'] = $address->street ? $address->street . ' ' : '';
+            $emailBody['house'] = $address->house ? $address->house . ' ' : '';
         } else {
-            $emailBody['area'] =  '';
-            $emailBody['block'] =  '';
+            $emailBody['area'] = '';
+            $emailBody['block'] = '';
             $emailBody['street'] = '';
-            $emailBody['house'] =  '';
+            $emailBody['house'] = '';
         }
 
 
-        $emailBody['email'] =  $this->order->email;
-        $emailBody['country'] =  $this->order->country->name;
-        $emailBody['currency'] =  $this->order->country->currency_en;
-        $emailBody['created_at'] =  $this->order->created_at->format('d-m-Y, h:i:s');
-        $emailBody['track_link'] =  route('order.track',['ref'=>$this->order->invoice_id]);
+        $emailBody['email'] = $this->order->email;
+        $emailBody['country'] = $this->order->country->name;
+        $emailBody['currency'] = $this->order->country->currency_en;
+        $emailBody['created_at'] = $this->order->created_at->format('d-m-Y, h:i:s');
+        $emailBody['track_link'] = route('order.track', ['ref' => $this->order->invoice_id]);
 
         foreach ($this->order->orderDetails as $orderDetail) {
-
             if ($orderDetail->product && $orderDetail->product->store) {
                 $emailBody['details'] = [];
                 $details = [];
@@ -96,47 +96,43 @@ class SendPaymentEmail
                 $details['sale_price'] = $orderDetail->sale_price;
                 $details['quantity'] = $orderDetail->quantity;
                 $details['delivery_date'] = $orderDetail->delivery_date;
-                $details['delivery_time'] = $orderDetail->deliveryDate ? $orderDetail->deliveryDate->name : '' ;
+                $details['delivery_time'] = $orderDetail->deliveryTime ? $orderDetail->deliveryTime->name : '';
                 $emailBody['details'][] = $details;
             }
         }
-
         $mailer->fire($emailBody);
     }
 
-    private function sendStoreCopy() {
-
+    private function sendStoreCopy()
+    {
         $mailer = $this->mailer;
-
         $mailer->view = 'emails.payment_success_store';
-
         $emailBody = [];
         $emailBody['invoice_id'] = $this->order->invoice_id;
-        $emailBody['name'] = $this->order->firstname . ' ' .$this->order->lastname;
+        $emailBody['name'] = $this->order->firstname . ' ' . $this->order->lastname;
         $emailBody['mobile'] = $this->order->mobile;
-        $emailBody['recipient_name'] = $this->order->recipient_firstname . ' ' .$this->order->recipient_lastname;
+        $emailBody['recipient_name'] = $this->order->recipient_firstname . ' ' . $this->order->recipient_lastname;
         $emailBody['recipient_mobile'] = $this->order->recipient_mobile;
         $emailBody['order_date'] = $this->order->created_at->format('d-m-Y');
         $emailBody['order_notes'] = $this->order->order_notes;
         $emailBody['card_notes'] = $this->order->card_notes;
-        if($address = $this->order->address) {
-            $emailBody['area'] =  $address->area ? $address->area->name : '-';
-            $emailBody['block'] =  $address->block ? $address->block : '';
-            $emailBody['street'] =  $address->street ? $address->street : '';
-            $emailBody['house'] =  $address->house ? $address->house : '';
+        if ($address = $this->order->address) {
+            $emailBody['area'] = $address->area ? $address->area->name . ' ' : '-';
+            $emailBody['block'] = $address->block ? $address->block . ' ' : '';
+            $emailBody['street'] = $address->street ? $address->street . ' ' : '';
+            $emailBody['house'] = $address->house ? $address->house : '';
         } else {
-            $emailBody['area'] =  '';
-            $emailBody['block'] =  '';
+            $emailBody['area'] = '';
+            $emailBody['block'] = '';
             $emailBody['street'] = '';
-            $emailBody['house'] =  '';
+            $emailBody['house'] = '';
         }
-        $emailBody['email'] =  $this->order->email;
-        $emailBody['country'] =  $this->order->country->name;
-        $emailBody['created_at'] =  $this->order->created_at->format('d-m-Y, h:i:s');
+        $emailBody['email'] = $this->order->email;
+        $emailBody['country'] = $this->order->country->name;
+        $emailBody['created_at'] = $this->order->created_at->format('d-m-Y, h:i:s');
 
         foreach ($this->order->orderDetails as $orderDetail) {
-
-            if($orderDetail->product && $orderDetail->product->store) {
+            if ($orderDetail->product && $orderDetail->product->store) {
                 $store = $orderDetail->product->store;
 
                 $emailBody['details'] = [];
@@ -147,12 +143,12 @@ class SendPaymentEmail
                 $details['sale_price'] = $orderDetail->sale_price;
                 $details['quantity'] = $orderDetail->quantity;
                 $details['delivery_date'] = $orderDetail->delivery_date;
-                $details['delivery_time'] = $orderDetail->deliveryDate ? $orderDetail->deliveryDate->name : '' ;
+                $details['delivery_time'] = $orderDetail->deliveryTime ? $orderDetail->deliveryTime->name : '';
 
                 $emailBody['details'][] = $details;
 
-                $mailer->subject =  '#'.$orderDetail->id.' '.trans('general.new_order') ;
-                $mailer->toEmail =  $store->email;
+                $mailer->subject = '#' . $orderDetail->id . ' ' . trans('general.new_order');
+                $mailer->toEmail = $store->email;
                 $mailer->toName = $store->name;
                 $mailer->fire($emailBody);
             }
